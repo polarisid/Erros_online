14ea2d9c4bc3e684c9cec71832c84736027d7b33
polarisid <danielcarvalhorst@gmail.com>
2025-04-20 22:07:39 -0300

    Vercel bug 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
+  build: {
+    outDir: 'dist'
+  },
+  base: './' // ESSENCIAL para funcionar no Vercel!
 })
